aa0bbf920c58bea75c5876d3bd66d73ba406a4e3
Brian N. Dady <bndady@gmail.com>
2020-12-01 02:01:06 +0000

    Adds serverless config and refactors engine to return a function

--- index.php
+++ index.php
@@ -2,40 +2,42 @@
 
 declare(strict_types=1);
 
-$width = (int) $_GET['width'];
-$height = (int) $_GET['height'];
+return function ($event) {
+    $width = (int) $event['width'];
+    $height = (int) $event['height'];
 
-$image = new Imagick('quiche.jpg');
+    $image = new Imagick('quiche.jpg');
 
-$ratio = $width / $height;
+    $ratio = $width / $height;
 
-// Original image dimensions
-$original_width = $image->getImageWidth();
-$original_height = $image->getImageHeight();
-$original_ratio = $original_width / $original_height;
+    // Original image dimensions
+    $original_width = $image->getImageWidth();
+    $original_height = $image->getImageHeight();
+    $original_ratio = $original_width / $original_height;
 
-// Determine new image dimensions to scale to.
-// Also determine cropping coordinates.
-if ($ratio > $original_ratio) {
-    $new_width = $width;
-    $new_height = $width / $original_width * $original_height;
-    $crop_x = 0;
-    $crop_y = intval(($new_height - $height) / 2);
-} else {
-    $new_width = $height / $original_height * $original_width;
-    $new_height = $height;
-    $crop_x = intval(($new_width - $width) / 2);
-    $crop_y = 0;
-}
+    // Determine new image dimensions to scale to.
+    // Also determine cropping coordinates.
+    if ($ratio > $original_ratio) {
+        $new_width = $width;
+        $new_height = $width / $original_width * $original_height;
+        $crop_x = 0;
+        $crop_y = intval(($new_height - $height) / 2);
+    } else {
+        $new_width = $height / $original_height * $original_width;
+        $new_height = $height;
+        $crop_x = intval(($new_width - $width) / 2);
+        $crop_y = 0;
+    }
 
-// Scale image to fit minimal of provided dimensions.
-$image->scaleImage((int) $new_width, (int) $new_height, true);
+    // Scale image to fit minimal of provided dimensions.
+    $image->scaleImage((int) $new_width, (int) $new_height, true);
 
-// Now crop image to exactly fit provided dimensions.
-$image->cropImage($width, $height, $crop_x, $crop_y);
+    // Now crop image to exactly fit provided dimensions.
+    $image->cropImage($width, $height, $crop_x, $crop_y);
 
-$image_blob = $image->getImageBlob();
+    $image_blob = $image->getImageBlob();
 
-$image->clear();
+    $image->clear();
 
-echo base64_encode($image_blob);
+    return base64_encode($image_blob);
+};
